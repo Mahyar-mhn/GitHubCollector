ce6cfff728c9fbaef854e2a67e64848b8208f189
Mahyar_Mohammadian <mhn.mahyarmohammadian1383@gmail.com>
2024-03-19 20:22:32 +0330

    saving the results in the output file

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def get_repositories(language, num_repo):
             data = response.json()
             page_repos = data['payload']['results'][:num_repo]
             repositories.extend(page_repos)
-            pprint(page_repos)
             page += 1
         else:
             print(f"Error: {response.status_code}")
@@ -26,4 +25,19 @@ language = input("Please enter your desired language: ")
 num_repo = int(input("Please enter number of repositories: "))
 
 repositories = get_repositories(language, num_repo)
+if repositories:
+    output = f"{language}_repositories.txt"
+    with open(output, mode="w", encoding="utf-8") as file:
+        file.write(f"Top {num_repo} repositories:\n")
+        file.write(50 * "*")
+        file.write(f"\n")
 
+        for i, repo in enumerate(repositories, start=1):
+            file.write(f"#{i} {repo['hl_name']} - {repo['hl_trunc_description']}\n"
+                       f"URL: https://github.com/{repo['repo']['repository']['owner_login']}"
+                       f"/{repo['repo']['repository']['name']}\n")
+            file.write(50 * "-")
+            file.write(f"\n")
+        print(f"Results saved to '{output}")
+else:
+    print("No repositories found!")
